df70876348dd4d0b847267e5900a4a482e614a7c
João de Freitas B Junior (Juninho) <joaofbjunior01@gmail.com>
2024-05-29 12:09:43 -0300

    Feature Que Vai Bugar (#6)
    * feat: :fire: remove hero
    
    * feat: remove links

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -12,17 +12,7 @@ const { show_login_window, authenticated } = storeToRefs(authStore);
 </script>
 <template>
   <main class="grid place-items-center pt-16 pb-8 md:pt-8 lg:grid-cols-2">
-    <div class="z-0 order-1 pt-5 sm:block sm:order-1 md:p-24 md:order-1 md:block">
-      <img
-        class="rounded-full transition-transform transform hover:scale-105 sm:hover:scale-125"
-        src="~/assets/img/logo.png"
-        alt="Starship starts the engine"
-        loading="eager"
-        format="avif"
-        width="400"
-        height="400"
-      />
-    </div>
+    <div class="z-0 order-1 pt-5 sm:block sm:order-1 md:p-24 md:order-1 md:block"></div>
     <div v-if="show_login_window" class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
       <LandingLogin></LandingLogin>
     </div>
@@ -32,16 +22,7 @@ const { show_login_window, authenticated } = storeToRefs(authStore);
         Amando a Deus e as pessoas<br />
         Servindo a Deus e as pessoas.
       </p>
-      <div class="flex mt-6 gap-3 flex-col sm:flex-row">
-        <template v-for="link in links">
-          <a
-            :href="link.href"
-            class="px-5 py-2.5 text-black bg-white border-2 border-black rounded text-center transition hover:bg-gray-100 focus-visible:ring-2 ring-offset-2 ring-gray-200"
-            :class="{ hidden: link.auth && !authenticated }"
-            >{{ link.text }}</a
-          >
-        </template>
-      </div>
+      <div class="flex mt-6 gap-3 flex-col sm:flex-row"></div>
     </div>
   </main>
 </template>
