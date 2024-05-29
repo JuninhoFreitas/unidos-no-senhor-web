165b2d4445bf4cb18ddd335dab5796cb72a04712
Juninho Freitas <joaofbjunior01@gmail.com>
2024-05-28 21:57:53 -0300

    feat: :sparkles: add key interactions in login

--- components/landing/Login.vue
+++ components/landing/Login.vue
@@ -1,40 +1,67 @@
 <script setup>
 import { ref } from 'vue';
 import { useMyAuthStore } from '~/store/auth';
+
 const email = ref('');
 const password = ref('');
 
 const storeAuth = useMyAuthStore();
 const { wrong_credentials } = storeToRefs(storeAuth);
 
-
-async function submit(){
+async function submit() {
   const payload = {
     email: email.value,
-    password: password.value
-  }
-  console.log('payload', payload)
+    password: password.value,
+  };
   await storeAuth.login(payload);
-  close();
+  if (!wrong_credentials) close();
 }
-
 </script>
 <template>
-  <div class="bg-white p-8 rounded-lg shadow-lg">
+  <div class="bg-white p-8 rounded-lg shadow-lg" @keyup.esc="storeAuth.close">
     <h2 class="text-2xl font-bold text-center">Login</h2>
     <form class="mt-4">
       <div class="mb-4">
         <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-        <input v-model="email" type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500" />
+        <input
+          v-model="email"
+          type="email"
+          id="email"
+          name="email"
+          class="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
+        />
       </div>
       <div class="mb-4">
         <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Senha</label>
-        <input v-model="password" type="password" id="password" name="password" class="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500" />
+        <input
+          v-model="password"
+          type="password"
+          id="password"
+          name="password"
+          class="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
+          @keyup.enter="submit"
+        />
       </div>
       <div class="flex justify-center">
-        <button @click="submit" type="button" class="bg-black text-white font-bold py-2 px-4 rounded-lg hover:bg-white hover:text-black hover:border-black border-2">Entrar</button>
+        <button
+          @click="submit"
+          type="button"
+          class="bg-black text-white font-bold py-2 px-4 rounded-lg hover:bg-white hover:text-black hover:border-black border-2"
+        >
+          Entrar
+        </button>
       </div>
     </form>
+    <UAlert
+      v-if="wrong_credentials"
+      icon="i-heroicons-command-line"
+      color="red"
+      variant="outline"
+      title="Tente novamente..."
+      description="Informações de Login incorretas"
+      type="error"
+      class="mt-4"
+    />
     <div class="mt-4 flex justify-center">
       <button @click="storeAuth.close" class="text-blue-500 hover:text-blue-700">Fechar</button>
     </div>
--- store/auth.js
+++ store/auth.js
@@ -1,37 +1,36 @@
 import { defineStore } from 'pinia';
 
-export const useMyAuthStore = defineStore('myAuthStore', ()=>{
+export const useMyAuthStore = defineStore('myAuthStore', () => {
   const config = useRuntimeConfig();
-  const authenticated = ref(false)
+  const authenticated = ref(false);
   const loading = ref(false);
-  const show_login_window = ref(false)
-  const wrong_credentials = ref(false)
-  const user = ref(null)
-  const roles = ref([])
+  const show_login_window = ref(false);
+  const wrong_credentials = ref(false);
+  const user = ref(null);
+  const roles = ref([]);
 
   async function getRoles() {
     const response = await fetch(`${config.public.baseUrl}/token/roles`, {
       method: 'get',
       headers: {
-         'Content-Type': 'application/json',
-         'Authorization': `Bearer ${useCookie('token').value}`,
-        },
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${useCookie('token').value}`,
+      },
     });
     const status = response.status;
     switch (status) {
       case 200:
-          const data = await response.json();
-          roles.value = data;
-          return data;
+        const data = await response.json();
+        roles.value = data;
+        return data;
       case 204:
         return [];
       default:
         break;
     }
-
   }
 
-  async function login ({ email, password }){
+  async function login({ email, password }) {
     const { data, pending, error } = await useFetch(`${config.public.baseUrl}/usuario/login`, {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
@@ -48,14 +47,16 @@ export const useMyAuthStore = defineStore('myAuthStore', ()=>{
       }); // useCookie new hook in nuxt 3
       token.value = data?.value?.access_token; // set token to cookie
       this.authenticated = true; // set authenticated  state value to true
+      wrong_credentials.value = false;
       await getRoles();
     }
-    if(error) {
-      console.log('Email ou senha inválidos');
+    if (error.value) {
+      console.log('Email ou senha inválidos', error);
       wrong_credentials.value = true;
     }
-    close();
-  };
+    console.log('wrong', wrong_credentials.value);
+    if (!wrong_credentials.value) close();
+  }
 
   function logout() {
     const token = useCookie('token'); // useCookie new hook in nuxt 3
@@ -67,5 +68,5 @@ export const useMyAuthStore = defineStore('myAuthStore', ()=>{
     show_login_window.value = false;
   };
 
-  return { login, show_login_window, authenticated, user, close, wrong_credentials, loading, logout, getRoles, roles }
-})
+  return { login, show_login_window, authenticated, user, close, wrong_credentials, loading, logout, getRoles, roles };
+});
