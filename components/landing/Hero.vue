bb828754c5b78c3cd2e3d531443bb00684a51f3c
Juninho Freitas <joaofbjunior01@gmail.com>
2024-05-29 12:07:15 -0300

    feat: :fire: remove hero

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -12,17 +12,7 @@ const { show_login_window, authenticated } = storeToRefs(authStore);
 </script>
 <template>
   <main class="grid place-items-center pt-16 pb-8 md:pt-8 lg:grid-cols-2">
-    <div class="z-0 order-1 pt-5 sm:block sm:order-1 md:p-24 md:order-1 md:block">
-      <img
-        class="rounded-full transition-transform transform hover:scale-105 sm:hover:scale-125"
-        src="~/assets/img/logo.png"
-        alt="Starship starts the engine"
-        loading="eager"
-        format="avif"
-        width="400"
-        height="400"
-      />
-    </div>
+    <div class="z-0 order-1 pt-5 sm:block sm:order-1 md:p-24 md:order-1 md:block"></div>
     <div v-if="show_login_window" class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
       <LandingLogin></LandingLogin>
     </div>
