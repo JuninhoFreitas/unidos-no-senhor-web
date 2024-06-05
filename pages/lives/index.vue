e31dbd333e2b6e86a53d0ca90c2384867318060e
Juninho Freitas <joaofbjunior01@gmail.com>
2024-06-04 23:55:52 -0300

    wip: teste

---
+++ pages/lives/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <VJitsiMeeting :domain="meet.jit.si" :room-name="teste" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+// Import Jitsi Meet API
+</script>
+
+<style></style>
